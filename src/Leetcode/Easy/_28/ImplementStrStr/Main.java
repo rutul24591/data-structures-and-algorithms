67cb0bf38abd8fbac18744bb0fc0cdbf88b6d327
Rutul Amin <rutulamin.ra@gmail.com>
2022-06-23 02:57:28 +0530

    Implemented problem 28. ImplementstrStr()

---
+++ src/Leetcode/Easy/_28/ImplementStrStr/Main.java
@@ -0,0 +1,11 @@
+package Leetcode.Easy._28.ImplementStrStr;
+
+public class Main {
+    public static void main(String[] args){
+        ImplementStrStr result = new ImplementStrStr();
+        String s1 = "hello";
+        String s2 = "o";
+
+        System.out.print("RESULT: " +result.strStr(s1, s2));
+    }
+}
