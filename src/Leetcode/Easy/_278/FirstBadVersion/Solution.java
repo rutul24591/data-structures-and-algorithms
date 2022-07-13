9dba683a49af82593304e12b520ce911c2086cee
Rutul Amin <rutulamin.ra@gmail.com>
2022-07-14 03:23:07 +0530

    implemented problem 278. First Bad Version

---
+++ src/Leetcode/Easy/_278/FirstBadVersion/Solution.java
@@ -0,0 +1,60 @@
+package Leetcode.Easy._278.FirstBadVersion;
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+/*
+        @author: Rutul Amin
+        @tag: Binary Search, Arrays
+        @date: 14th July 2022
+
+        278. First Bad Version
+
+        You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+        Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+        You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+        Example 1:
+            Input: n = 5, bad = 4
+            Output: 4
+            Explanation:
+            call isBadVersion(3) -> false
+            call isBadVersion(5) -> true
+            call isBadVersion(4) -> true
+            Then 4 is the first bad version.
+
+        Example 2:
+            Input: n = 1, bad = 1
+            Output: 1
+
+
+        Constraints:
+            1 <= bad <= n <= 231 - 1
+
+        Submission Details:
+            Runtime: 23 ms, faster than 41.56% of Java online submissions for First Bad Version.
+            Memory Usage: 41.3 MB, less than 20.48% of Java online submissions for First Bad Version.
+
+        Time: O(logn)
+        Space: O(1)
+*/
+
+public class Solution extends VersionControl{
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        int res = -1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(isBadVersion(mid)){
+                res = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/Leetcode/Easy/_278/FirstBadVersion/VersionControl.java
@@ -0,0 +1,12 @@
+package Leetcode.Easy._278.FirstBadVersion;
+
+public class VersionControl {
+    public boolean isBadVersion(int n){
+        /* Something */
+        int num = 14;
+        if(n == num){
+            return true;
+        }
+        return false;
+    }
+}
